cd69932ad349a844528ff9d9bb362dbc04b26585
No-si-wq <owendejesus323@gmail.com>
2026-06-26 13:24:17 -0600

    errpr

--- frontend/src/modules/quotations/pages/Quotations.tsx
+++ frontend/src/modules/quotations/pages/Quotations.tsx
@@ -142,7 +142,7 @@ export default function Quotations() {
         priceListId: values.priceListId,
         sellerId: values.sellerId,
         observations: values.observations,
-        expiresAt: values.expiresAt?.endOf('day').toISOString(),
+        expiresAt: expiresAt: values.expiresAt?.add(1, 'day').startOf('day').toISOString(),,
         items: cart.map((i) => ({
           productId: i.productId,
           quantity: i.quantity,
